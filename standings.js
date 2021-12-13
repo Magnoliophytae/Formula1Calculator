f918b541c7e71f32d3ae39110eaccf4f9bffc684
Ruelf <testingstuffuser@gmail.com>
2021-12-13 09:05:22 +0100

    Rename param

--- standings.js
+++ standings.js
@@ -3,9 +3,9 @@ const url = "https://ergast.com/api/f1/current/driverStandings.json";
 
 fetch(url).then(response => {
     response.json().then(data => {
-        data.MRData.StandingsTable.StandingsLists[0].DriverStandings.forEach((x, index) => {
+        data.MRData.StandingsTable.StandingsLists[0].DriverStandings.forEach((x, i) => {
             const row = table.insertRow();
-            row.insertCell().textContent = `${index + 1}. ${x.Driver.givenName} ${x.Driver.familyName}`;
+            row.insertCell().textContent = `${i + 1}. ${x.Driver.givenName} ${x.Driver.familyName}`;
             row.insertCell().textContent = x.points;
         });
     });
