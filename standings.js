ec8727039e70031d839433dd5e8d83aecc67c875
Ruelf <testingstuffuser@gmail.com>
2021-12-13 09:17:20 +0100

    Change standings title

--- standings.js
+++ standings.js
@@ -1,8 +1,10 @@
 const table = document.getElementById("standings");
+const title = document.getElementById("s-title");
 const url = "https://ergast.com/api/f1/current/driverStandings.json";
 
 fetch(url).then(response => {
     response.json().then(data => {
+        title.textContent = `${data.MRData.StandingsTable.season} Drivers' Standings`;
         data.MRData.StandingsTable.StandingsLists[0].DriverStandings.forEach((x, i) => {
             const row = table.insertRow();
             row.insertCell().textContent = `${i + 1}. ${x.Driver.givenName} ${x.Driver.familyName}`;
