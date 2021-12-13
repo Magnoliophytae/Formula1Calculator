3f8a48b8178c97ea0833ea70a590b7dfdf221313
Juno <77465215+Magnoliophytae@users.noreply.github.com>
2021-12-13 02:10:52 +0100

    s

--- standings.js
+++ standings.js
@@ -1,5 +1,5 @@
 const table = document.getElementById("standings");
-const url = "http://ergast.com/api/f1/current/driverStandings.json";
+const url = "https://ergast.com/api/f1/current/driverStandings.json";
 
 fetch(url).then(response => {
     response.json().then(data => {
